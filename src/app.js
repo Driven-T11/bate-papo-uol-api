171f567061ded3ece635c14ea9e0c51d2d697264
leticia-chijo <leticia.chijo@gmail.com>
2023-07-03 19:45:33 -0300

    Requisito: POST /participants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import cors from "cors"
 import express from "express"
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
+import Joi from "joi"
+import dayjs from "dayjs"
 
 const app = express()
 
@@ -12,6 +14,7 @@ dotenv.config()
 
 // Conexão DB
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
+
 try {
     mongoClient.connect()
     console.log("MongoDB conectado!")
@@ -22,8 +25,38 @@ try {
 const db = mongoClient.db()
 
 // Schemas
+const participantSchema = Joi.object({ name: Joi.string().required() })
 
 // Endpoints
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+
+    const validation = participantSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        return res.status(422).send(validation.error.details.map(detail => detail.message))
+    }
+
+    try {
+        const participant = await db.collection('participants').findOne({ name })
+        if (participant) return res.sendStatus(409)
+
+        const timestamp = Date.now()
+        await db.collection('participants').insertOne({ name, lastStatus: timestamp })
+
+        const message = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(timestamp).format('HH:mm:ss')
+        }
+        await db.collection('messages').insertOne(message)
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 
 // Deixa o app escutando, à espera de requisições
 const PORT = 5000
